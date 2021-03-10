f8619cb92f300b4fb5b1b327c1a2da1c53cc8536
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-03-10 12:57:12 -0500

    Create CogManager.py

---
+++ CogManager.py
@@ -0,0 +1,90 @@
+'''
+SETUP:
+
+All you need to do is add a `cogs` folder and move all of your cog files there, thats it!
+
+'''
+
+
+
+def get_extensions():  # Gets extension list dynamically
+    extensions = []
+    for file in Path("cogs").glob("**/*.py"):
+        if "!" in file.name or "__" in file.name:
+            continue
+        extensions.append(str(file).replace("/", ".").replace(".py", ""))
+    return extensions 
+  
+for ext in get_extensions(): #Loads every cog in your cog folder
+    client.load_extension(ext)
+  
+@client.group(aliases=['cog'])
+@commands.is_owner()
+async def cogs(ctx):
+    pass
+
+
+@cogs.command()
+@commands.is_owner()
+async def unload(ctx, ext):
+    if "cogs." not in ext:
+        ext = f"cogs.{ext}"
+    if ext in get_extensions():
+        client.unload_extension(ext)
+        embed = discord.Embed(
+            title="Cogs - Unload", description=f"Unloaded cog: {ext}", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="Cogs Reloaded", description=f"Cog '{ext}' not found", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+
+
+@cogs.command()
+@commands.is_owner()
+async def load(ctx, ext):
+    if "cogs." not in ext:
+        ext = f"cogs.{ext}"
+    if ext in get_extensions():
+        client.load_extension(ext)
+        embed = discord.Embed(title="Cogs - Load",
+                              description=f"Loaded cog: {ext}", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="Cogs - Load", description=f"Cog '{ext}' not found.", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+
+
+@cogs.command(aliases=['restart'])
+@commands.is_owner()
+async def reload(ctx, ext):
+    if ext == "all":
+        embed = discord.Embed(
+            title="Cogs - Reload", description="Reloaded all cogs", color=0xd6b4e8)
+        for extension in get_extensions():
+            client.reload_extension(extension)
+        await ctx.send(embed=embed)
+        return
+
+    if "cogs." not in ext:
+        ext = f"cogs.{ext}"
+
+    if ext in get_extensions():
+        client.reload_extension(ext)
+        embed = discord.Embed(
+            title="Cogs - Reload", description=f"Reloaded cog: {ext}", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+
+    else:
+        embed = discord.Embed(
+            title="Cogs - Reload", description=f"Cog '{ext}' not found.", color=0xd6b4e8)
+        await ctx.send(embed=embed)
+
+
+@cogs.command()
+@commands.is_owner()
+async def view(ctx):
+    msg = " ".join(get_extensions())
+    embed = discord.Embed(title="Cogs - View", description=msg, color=0xd6b4e8)
+    await ctx.send(embed=embed)
